4b0fa0a58bdb221868f26f42cc51fcad0bf1c367
murffious <murff.paul@gmail.com>
2021-02-09 21:48:53 -0700

    env

--- dynamodb-lambda/app.py
+++ dynamodb-lambda/app.py
@@ -3,9 +3,11 @@
 from aws_cdk import core
 
 from dynamodb_lambda.dynamodb_lambda_stack import DynamodbLambdaStack
-
+from pipeline.pipeline_stack import MyPipelineStack
 
 app = core.App()
 DynamodbLambdaStack(app, "dynamodb-lambda")
-
+MyPipelineStack(
+    app, "my-pipeline", env=core.Environment(account="111111111111", region="eu-west-1")
+)
 app.synth()
